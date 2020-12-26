70f6fa4e29f613eeb4aeab63ea18fce1ffe76db1
Alice <am171696@alunos.unisanta.br>
2020-12-26 19:17:57 -0300

    Inserindo produto na tabela produto com o PreparedStatement para evitar o SQL Injection

--- src/TestaInsercao.java
+++ src/TestaInsercao.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -6,12 +7,19 @@ import java.sql.Statement;
 public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
+		String nome = "Mouse'";
+		String descricao = "Mouse em fio'); DELETE FROM PRODUTO;";
+		
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection con = factory.conectar();
+		PreparedStatement pst = con.prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES ( ? , ?)", Statement.RETURN_GENERATED_KEYS);
 		
-		Statement stm = con.createStatement();
-		stm.execute("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES ('MOUSE', 'MOUSE SEM FIO')", Statement.RETURN_GENERATED_KEYS);
-		ResultSet rs = stm.getGeneratedKeys();
+		pst.setString(1, nome);
+		pst.setString(2, descricao);
+		
+		pst.execute();
+		
+		ResultSet rs = pst.getGeneratedKeys();
 		while(rs.next()) {
 			int id = rs.getInt(1);
 			System.out.println(id);
