f5928dde1834a3e5655a4b006befd4f7630e4b40
Alice <am171696@alunos.unisanta.br>
2020-12-26 13:37:22 -0300

    Utilizando ConnectionFactory para obter a Connection

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -1,12 +1,13 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestaConexao {
 
 	public static void main(String[] args) throws SQLException {
 
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection conn = connectionFactory.conectar();
+		
 		System.out.println("Fechando conexão");
 		conn.close();
 	}
--- src/TestaListagem.java
+++ src/TestaListagem.java
@@ -1,5 +1,4 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,7 +7,8 @@ public class TestaListagem {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection con = connectionFactory.conectar();
 		
 		Statement stm = con.createStatement();
 		stm.execute("SELECT * FROM PRODUTO");
