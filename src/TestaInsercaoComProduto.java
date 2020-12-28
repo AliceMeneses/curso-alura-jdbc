db4bab4819ae05ecc44d73607dce9f5d69e2e6fb
Alice <am171696@alunos.unisanta.br>
2020-12-28 17:20:32 -0300

    Inserindo produtos atraves do ProdutoDAO

--- src/TestaInsercaoComProduto.java
+++ src/TestaInsercaoComProduto.java
@@ -8,21 +8,11 @@ public class TestaInsercaoComProduto {
 
 	public static void main(String[] args) throws SQLException {
 		Produto sofa = new Produto("sofa", "sofa de canto");
-		
-		String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?,?)";
-		
+				
 		ConnectionFactory cf = new ConnectionFactory();
 		try(Connection con = cf.conectar()){
-			try(PreparedStatement pstm = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
-				pstm.setString(1, sofa.getNome());
-				pstm.setNString(2, sofa.getDescricao());
-				pstm.execute();
-				try(ResultSet rs = pstm.getGeneratedKeys()){
-					while(rs.next()) {
-						sofa.setId(rs.getInt(1));
-					}
-				}
-			}
+			ProdutoDAO produtoDAO = new ProdutoDAO(con);
+			produtoDAO.salvar(sofa);
 		}
 		
 		System.out.println(sofa);
