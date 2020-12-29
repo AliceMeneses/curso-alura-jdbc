633c98a4e27d7a78772ad1c3ece8475ac8dd49fd
Alice <am171696@alunos.unisanta.br>
2020-12-29 16:27:10 -0300

    Adicionando listarComProdutos() que realiza um INNER JOIN

--- src/CategoriaDAO.java
+++ src/CategoriaDAO.java
@@ -7,27 +7,58 @@ import java.util.List;
 
 public class CategoriaDAO {
 	private Connection connection;
-	
+
 	public CategoriaDAO(Connection connection) {
 		this.connection = connection;
 	}
-	
-	public List<Categoria> listar() throws SQLException{
+
+	public List<Categoria> listar() throws SQLException {
 		List<Categoria> categorias = new ArrayList<Categoria>();
-		
+
 		String sql = "SELECT * FROM CATEGORIA";
-		
-		try(PreparedStatement pstm =  connection.prepareStatement(sql)){
+
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
 			pstm.execute();
-			
-			try(ResultSet rs = pstm.getResultSet()){
-				while(rs.next()) {
+
+			try (ResultSet rs = pstm.getResultSet()) {
+				while (rs.next()) {
 					Categoria categoria = new Categoria(rs.getInt(1), rs.getString(2));
 					categorias.add(categoria);
 				}
 			}
 		}
+
+		return categorias;
+	}
+
+	public List<Categoria> listarComProdutos() throws SQLException {
 		
+		Categoria ultima = null;
+		List<Categoria> categorias = new ArrayList<Categoria>();
+
+		String sql = "SELECT * FROM CATEGORIA C INNER JOIN PRODUTO P ON C.ID = P.CATEGORIA_ID;";
+
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.execute();
+
+			try (ResultSet rs = pstm.getResultSet()) {
+				
+				while (rs.next()) {
+					if(ultima == null || !ultima.getNome().equals(rs.getString(2))) {
+						Categoria categoria = new Categoria(rs.getInt(1), rs.getString(2));
+						ultima = categoria;
+						categorias.add(categoria);
+
+					}
+					
+					Produto produto = new Produto(rs.getInt(3), rs.getString(4), rs.getString(5));
+					ultima.adicionar(produto);
+					
+					
+				}
+			}
+		}
+
 		return categorias;
 	}
 }
