2181893707c24b89700f6e7e694d3131594c14b7
Alice <am171696@alunos.unisanta.br>
2020-12-29 16:27:59 -0300

    Adicionando lista de produtos e seu get e set

--- src/Categoria.java
+++ src/Categoria.java
@@ -1,16 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Categoria {
 	
 	private int id;
 	private String nome;
+	private List<Produto> produtos;
 	
 	public Categoria(int id, String nome) {
 		this.id = id;
 		this.nome = nome;
+		this.produtos = new ArrayList<Produto>();
+		
 	}
 	
 	@Override
 	public String toString() {
 		return String.format("CATEGORIA: %d, %s", this.id, this.nome);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void adicionar(Produto produto) {
+		produtos.add(produto);
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
 	}
 }
